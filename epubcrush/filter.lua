43b58628a87a3503ecd4dece38aab21fc6269198
Jon Craton <jncraton@gmail.com>
2025-02-07 22:36:16 -0500

    Add epubcrush/filter.lua

---
+++ epubcrush/filter.lua
@@ -0,0 +1,74 @@
+-- Remove all images
+function Image(el)
+  print("Removing image", el)
+  return {}
+end
+
+-- Remove empty spans
+-- Remove classes from all spans
+function Span(el)
+  if #el.content == 0 then
+    print("Removing span", el)
+    print(#el.content)
+    return {}
+  end
+
+  el.classes = pandoc.List{}
+  return el
+end
+
+-- Unwrap divs
+function Div(el)
+  print("Unwrapping fenced div", el)
+  return el.content
+end
+
+-- Remove raw HTML blocks
+function RawBlock(el)
+  if el.format == "html" then
+    return {}
+  end
+end
+
+-- Remove raw HTML inlines
+function RawInline(el)
+  if el.format == "html" then
+    return {}
+  end
+end
+
+
+-- Remove all links
+function Link(el)
+  return el.content
+end
+
+function Header(el)
+end
+
+
+-- Ensure headers start at level 1
+-- Clear all classes from headers
+local has_level_1 = false
+
+function Header(el)
+  if el.level == 1 then
+    has_level_1 = true
+  end
+  el.classes = pandoc.List{}
+  return el
+end
+
+function Pandoc(doc)
+  if not has_level_1 then
+    return doc:walk {
+      Header = function(el)
+        if el.level > 1 then
+          el.level = el.level - 1
+        end
+        return el
+      end
+    }
+  end
+end
+
