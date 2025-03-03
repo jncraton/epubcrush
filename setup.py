eca0a9a16d3bcf27c4f07455d2b38d949fe2a049
Jon Craton <jncraton@gmail.com>
2025-03-02 19:38:10 -0500

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as fh:
 
 setuptools.setup(
     name="epubcrush",
-    version="3.1.1",
+    version="3.1.2",
     author="Jon Craton",
     author_email="jon@joncraton.com",
     description="Compress EPUB files",
